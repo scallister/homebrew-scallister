444c86dabf588c1545fb2e9748ec310fd15780e5
scallister <32499763+scallister@users.noreply.github.com>
2026-07-14 19:45:50 +0000

    chore: bump formulae

--- Formula/wemowatch.rb
+++ Formula/wemowatch.rb
@@ -3,10 +3,9 @@ class Wemowatch < Formula
   desc "Wemowatch"
   homepage "https://github.com/scallister/wemowatch"
   url "https://github.com/scallister/wemowatch.git",
-    tag: "v1.0.4",
+    tag: "v1.0.5",,
     :using => :git
-
-  version "v1.0.4"
+  version "v1.0.5"
 
   depends_on "go" => :build
 
